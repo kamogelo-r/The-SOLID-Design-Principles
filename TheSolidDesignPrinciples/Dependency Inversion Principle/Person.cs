f3d7470bd9991f71de02103aa41c53d8af623226
kamogelo-r <kamo.ramokgadi@gmail.com>
2020-02-12 11:44:43 +0200

    Show when the principle is needed

---
+++ TheSolidDesignPrinciples/Dependency Inversion Principle/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheSolidDesignPrinciples.Dependency_Inversion_Principle
+{
+    /// <summary>
+    /// high level parts of the system should not
+    /// depend on low level parts of the system
+    /// directly, but through abstraction instead
+    /// </summary>
+    public class Person
+    {
+        public string name;
+    }
+
+    /// <summary>
+    /// low-level
+    /// </summary>
+    public class Relationships
+    {
+        private List<(Person, Relationship, Person)> relations = new List<(Person, Relationship, Person)>();
+
+        public void AddParentChild(Person parent, Person child)
+        {
+            relations.Add((parent, Relationship.Parent, child));
+            relations.Add((parent, Relationship.Child, parent));
+        }
+    }
+
+    public enum Relationship
+    {
+        Parent, Child, Sibling
+    }
+}
--- TheSolidDesignPrinciples/Program.cs
+++ TheSolidDesignPrinciples/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TheSolidDesignPrinciples.Dependency_Inversion_Principle;
 
 namespace TheSolidDesignPrinciples
 {
@@ -10,6 +11,17 @@ namespace TheSolidDesignPrinciples
     {
         static void Main(string[] args)
         {
+            // Dependency Inversion Principle
+            var parent = new Person { name = "John" };
+            var child1 = new Person { name = "Chris" };
+            var child2 = new Person { name = "Mary" };
+
+            //access the low-level module in the high-level module
+            var relationships = new Relationships();
+            relationships.AddParentChild(parent, child1);
+            relationships.AddParentChild(parent, child2);
+
+            Console.ReadLine();
         }
     }
 }
